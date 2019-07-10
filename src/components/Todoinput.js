7079e10240c94027068d35736d216611529988a3
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-07-10 06:52:57 +0200

    Update

--- src/components/Todoinput.js
+++ src/components/Todoinput.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { classBody } from '@babel/types';
 
 export default class Todoinput extends Component {
     render() {
-        return (
-            <div>
-                hello from to do input
+        return <div className="card card-body my-3">
+        <form>
+            <div className="input-group">
+                <div className="input-group-prepend">
+                    <div className="input-group-text bg-primary text-white">
+                        <i className="fas fa-book">
+
+                        </i>
+                    </div>
+                </div>
+                <input type="text" classNam="form-control" placeholder="add a todo item"/>
             </div>
-        )
+        </form>  
+        </div>
+                
     }
 }
