4fd5d715b7bdfb754069200c1be39334f3203e18
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-07-10 07:37:05 +0200

    Update

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react'
 export default class TodoItem extends Component {
     render() {
         return (
-            <div>
-                hello from the item
-            </div>
+            
+            <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+                <h6>title</h6>
+                <div className="todo-icon">
+                    <span className="mx-2 text-success">
+                        <i className="fas fa-pen">
+                            
+                        </i>
+                    </span> 
+                </div>    
+            </li>
+            
         )
     }
 }
--- src/components/Todoinput.js
+++ src/components/Todoinput.js
@@ -4,18 +4,17 @@ import { classBody } from '@babel/types';
 export default class Todoinput extends Component {
     render() {
         return <div className="card card-body my-3">
-        <form>
-            <div className="input-group">
-                <div className="input-group-prepend">
-                    <div className="input-group-text bg-primary text-white">
-                        <i className="fas fa-book">
-
-                        </i>
+            <form>
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <div className="input-group-text bg-primary text-white">
+                            <i className="fas fa-book"/>
+                        </div>
                     </div>
+                    <input type="text" classNam="form-control" placeholder="add a todo item"/>
                 </div>
-                <input type="text" classNam="form-control" placeholder="add a todo item"/>
-            </div>
-        </form>  
+                <button type="submit" className="btn btn-block btn-primary mt-3">add item</button>
+            </form>  
         </div>
                 
     }
